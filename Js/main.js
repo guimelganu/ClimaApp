212853a10612ad84c94776f03344d75f2201930c
Thalia Olvera <thalia_olvera@hotmail.com>
2022-08-04 13:46:42 -0500

    Conexion de Api

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,34 @@ const toggle = document.querySelector(".toggle")
 const menuDashboard = document.querySelector(".menu-dashboard")
 const enlacesMenu = document.querySelectorAll(".enlace")
 
+/*THOR: Elementos para la conexion de la api */
+const button = document.querySelector('.button')
+const input = document.querySelector('.input')
+const name = document.querySelector ('.name');
+const desc = document.querySelector ('.desc');
+const temp = document.querySelector ('.temp');
+
+/*THOR: CONEXION A API CON FETCH PARA QUE MUESTRE TEMPERATURA, CIUDAD Y DESCRIPCION */
+button.addEventListener ('click',function(){
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=e118897ef97711f5e8168e4f854c5287')
+        .then(Response => Response.json())
+        .then (data =>{
+            var nameValue = data ['name'];
+            var tempValue = data ['main']['temp'];
+            var descValue = data ['weather'][0]['description'];
+
+           name.innerHTML = nameValue;
+           temp.innerHTML = tempValue;
+           desc.innerHTML = descValue;
+        })
+
+
+    .catch( err => alert("Error Nombre de ciudad"))
+})
+
+
+
 toggle.addEventListener("click", () => {
     menuDashboard.classList.toggle("open")
 
